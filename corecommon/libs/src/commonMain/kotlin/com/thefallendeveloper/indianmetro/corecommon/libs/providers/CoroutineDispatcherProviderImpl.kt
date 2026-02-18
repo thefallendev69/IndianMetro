fde4e0e2e307448826d79c0ccf54c9c16ff0bac7
thefallendev69 <the.fallen.developer@gmail.com>
2026-02-19 00:25:05 +0530

    corecommon-libs: add platform dispatchers for coroutine provider

--- corecommon/libs/src/commonMain/kotlin/com/thefallendeveloper/indianmetro/corecommon/libs/providers/CoroutineDispatcherProviderImpl.kt
+++ corecommon/libs/src/commonMain/kotlin/com/thefallendeveloper/indianmetro/corecommon/libs/providers/CoroutineDispatcherProviderImpl.kt
@@ -1,17 +1,11 @@
 package com.thefallendeveloper.indianmetro.corecommon.libs.providers
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 
-class CoroutineDispatcherProviderImpl : ICoroutineDispatchersProvider {
+expect class CoroutineDispatcherProviderImpl() : ICoroutineDispatchersProvider {
     override val main: CoroutineDispatcher
-        get() = Dispatchers.Main
     override val mainImmediate: CoroutineDispatcher
-        get() = Dispatchers.Main.immediate
     override val io: CoroutineDispatcher
-        get() = Dispatchers.Default
     override val default: CoroutineDispatcher
-        get() = Dispatchers.Default
     override val unConfined: CoroutineDispatcher
-        get() = Dispatchers.Unconfined
 }
