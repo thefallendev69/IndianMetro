b7d932ceeaf8741bb4c52f415ad787e493a24184
thefallendev69 <the.fallen.developer@gmail.com>
2026-02-25 00:14:26 +0530

    Added kover for code coverage.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,29 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
+    alias(libs.plugins.kover) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.indianmetroLintSuite)
 }
 
 apply(from = "$rootDir/gradle/ij-kotlin-mpp-test-logger-workaround.gradle")
+
+val koverPluginId = libs.plugins.kover.get().pluginId
+
+subprojects {
+    pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
+        afterEvaluate {
+            val hasBaseTestDependency =
+                configurations.any { configuration ->
+                    configuration.dependencies.any { dependency ->
+                        dependency is org.gradle.api.artifacts.ProjectDependency &&
+                            dependency.dependencyProject.path == ":corecommon:baseTest"
+                    }
+                }
+
+            if (hasBaseTestDependency && !pluginManager.hasPlugin(koverPluginId)) {
+                pluginManager.apply(koverPluginId)
+            }
+        }
+    }
+}
