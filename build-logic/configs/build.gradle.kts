890a34b6178005a7ecb0b2dead1622e7292e6eac
thefallendev69 <the.fallen.developer@gmail.com>
2026-02-25 17:09:47 +0530

    build-logic: move kover config to configs plugin and simplify CI gate flags

--- build-logic/configs/build.gradle.kts
+++ build-logic/configs/build.gradle.kts
@@ -16,6 +16,7 @@ val libsCatalog = extensions.getByType(VersionCatalogsExtension::class.java).nam
 dependencies {
     implementation(libsCatalog.findLibrary("kotlin-gradle-plugin").get())
     implementation(libsCatalog.findLibrary("android-gradle-plugin").get())
+    implementation(libsCatalog.findLibrary("kover-gradle-plugin").get())
 }
 
 gradlePlugin {
@@ -23,6 +24,10 @@ gradlePlugin {
         register("indianmetroJvmConfigConventions") {
             id = "com.thefallendeveloper.indianmetro.jvm-config-conventions"
             implementationClass = "com.thefallendeveloper.indianmetro.configs.JvmConfigConventionsPlugin"
+        }
+        register("indianmetroKoverConfigConventions") {
+            id = "com.thefallendeveloper.indianmetro.kover-config-conventions"
+            implementationClass = "com.thefallendeveloper.indianmetro.configs.KoverConfigConventionsPlugin"
         }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import kotlinx.kover.gradle.plugin.dsl.KoverProjectExtension
-
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
@@ -10,6 +8,7 @@ plugins {
     alias(libs.plugins.kover) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.indianmetroLintSuite)
+    alias(libs.plugins.indianmetroKoverConfigConventions)
 }
 
 apply(from = "$rootDir/gradle/ij-kotlin-mpp-test-logger-workaround.gradle")
@@ -29,21 +28,6 @@ subprojects {
 
             if (hasBaseTestDependency && !pluginManager.hasPlugin(koverPluginId)) {
                 pluginManager.apply(koverPluginId)
-            }
-        }
-    }
-
-    pluginManager.withPlugin(koverPluginId) {
-        extensions.configure<KoverProjectExtension> {
-            reports {
-                filters {
-                    excludes {
-                        packages(
-                            "*.di",
-                            "*.di.*",
-                        )
-                    }
-                }
             }
         }
     }
